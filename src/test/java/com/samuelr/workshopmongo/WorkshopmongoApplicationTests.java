c65899a3f6e88f22440b6d9daac67ac27cfc00e7
Samuel <rogenescarvalho@hotmail.com>
2023-12-22 14:35:32 -0300

    Created project

---
+++ src/test/java/com/samuelr/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelr.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
